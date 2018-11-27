1b544ecfdc786d8680344581db1099375362cd89
Dain <dain.park@my.jcu.edu.au>
2018-11-27 11:12:43 +0800

    initial commit

---
+++ prac_04/list_exercise_1.py
@@ -0,0 +1,11 @@
+
+numbers = []
+for i in range(5):
+    number = int(input("Numner: "))
+    numbers.append(number)
+
+print("The first number is: ", numbers[0])
+print("The last number is: ", numbers[-1])
+print("The smallest number is ", min(numbers))
+print("The largest number is ", max(numbers))
+print("The average of the number is ", sum(numbers) / len(numbers))
