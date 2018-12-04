1d98fec4c78db32a7ad4185a5d2a38e0ea833f7a
Dain <dain.park@my.jcu.edu.au>
2018-12-04 11:36:38 +0800

    initial commit

--- prac_04/list_exercise_2.py
+++ prac_04/list_exercise_2.py
@@ -1,9 +1,8 @@
-
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
-'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
-'InterpreterInterface', 'StartServer', 'bob']
-
-username = input("Enter username: ")
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+             'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command',
+             'ExecState', 'InteractiveConsole', 'InterpreterInterface',
+             'StartServer', 'bob']
+username = input("Enter username:")
 if username in usernames:
     print("Access granted")
 else:
